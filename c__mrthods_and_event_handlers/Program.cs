376944afc2277149b4251f5173c7ff1c62cd9cf6
jett <96192323+jertgfg@users.noreply.github.com>
2024-01-04 10:30:44 -0500

    Add files via upload

---
+++ c__mrthods_and_event_handlers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c__mrthods_and_event_handlers
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
